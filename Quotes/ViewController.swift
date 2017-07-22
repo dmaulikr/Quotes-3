d840bf02ec96975b2651330eaa3d08ca664a2071
Jonathan Soryu <jonathan.soryu@icloud.com>
2017-07-22 13:15:21 -0400

    Initial Commit

---
+++ Quotes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quotes
+//
+//  Created by Jonathan Soryu on 2017-07-22.
+//  Copyright © 2017 kernelPanic! Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
